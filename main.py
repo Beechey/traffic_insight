0f30c603d74668d8754dd95273984483866de44a
Matt Beechey <m.beechey@lboro.ac.uk>
2024-12-23 16:54:00 +0000

    Annotate classes

--- main.py
+++ main.py
@@ -1,37 +1,71 @@
 import argparse
+import cv2
 import numpy as np
 import supervision as sp
 import torch
 from ultralytics import YOLO
 
+COLOURS = sp.ColorPalette.DEFAULT
+
 
 class VideoProcessor:
     def __init__(
         self,
-        weights_path: str,
-        src_video_path: str,
-        output_video_path: str = None,
-        confidence_threshold: float = 0.3,
-        iou_threshold: float = 0.7,
+        weights: str,
+        video: str,
+        output: str = None,
+        conf: float = 0.3,
+        iou: float = 0.7,
         device: str = "cpu",
     ) -> None:
-        self.source_video = src_video_path
-        self.output_video = output_video_path
-        self.confidence_threshold = confidence_threshold
-        self.iou_threshold = iou_threshold
+        self.source_video = video
+        self.output_video = output
+        self.confidence_threshold = conf
+        self.iou_threshold = iou
         self.device = torch.device(device)
-        self.model = YOLO(weights_path, device=self.device)
+        self.model = YOLO(weights)
+        self.box_annotator = sp.BoxAnnotator(color=COLOURS)
+        self.label_annotator = sp.LabelAnnotator(color=COLOURS)
+        self.tracker = sp.ByteTrack()
 
     def process_video(self) -> None:
-        pass
+        frame_generator = sp.get_video_frames_generator(
+            source_path=self.source_video
+        )
+
+        for frame in frame_generator:
+            processed_frame = self.process_frame(frame=frame)
+            cv2.imshow("Frame", processed_frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+        cv2.destroyAllWindows()
 
     def annotate_frame(
         self, frame: np.ndarray, detections: sp.Detections
     ) -> np.ndarray:
-        pass
+        annotated_frame = frame.copy()
+        labels = [f"#{tracker_id}" for tracker_id in detections.tracker_id]
+        annotated_frame = self.box_annotator.annotate(
+            scene=annotated_frame, detections=detections
+        )
+        annotated_frame = self.label_annotator.annotate(
+            scene=annotated_frame, detections=detections, labels=labels
+        )
+        return annotated_frame
 
     def process_frame(self, frame: np.ndarray) -> np.ndarray:
-        pass
+        result = self.model(
+            frame,
+            verbose=False,
+            conf=self.confidence_threshold,
+            iou=self.iou_threshold,
+            device=self.device,
+        )[0]
+        detections = sp.Detections.from_ultralytics(result)
+        detections = self.tracker.update_with_detections(detections)
+        return self.annotate_frame(frame, detections)
 
 
 if __name__ == "__main__":
